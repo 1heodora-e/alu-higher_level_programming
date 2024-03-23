85b79bf624024a9d6d9e92981ef4127656513344
1hedoora-e <t.egbunike@alustudent.com>
2024-03-23 14:29:03 -0700

    yes

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,8 @@
--- a script that lists all privileges of the MySQL users:
--- user_0d_1 and user_0d_2
+-- Check if user 'user_0d_1' exists and list privileges
+SELECT COUNT(*) FROM mysql.user WHERE user = 'user_0d_1';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Check if user 'user_0d_2' exists and list privileges
+SELECT COUNT(*) FROM mysql.user WHERE user = 'user_0d_2';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
