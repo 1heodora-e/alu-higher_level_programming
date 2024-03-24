e59994cdf66b9426f0c4ee24664a6c2fc4d28c3d
1hedoora-e <t.egbunike@alustudent.com>
2024-03-24 05:55:13 -0700

    please work

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
-""" a Python script that fetches https://intranet.hbtn.io/status """
+'''
+This script fetches the status information from a URL and prints the results
+in a formatted way.
+
+Usage:
+    python fetch_status.py
+
+Dependencies:
+    - requests library (install using "pip install requests")
+
+Example Output:
+    Body response:
+        - type: <class 'bytes'>
+        - content: b'OK\n'
+        - utf8 content: OK
+'''
 import urllib.request
 
+if __name__ == "__main__":
+    '''
+    This script fetches the status information from different URLs and
+    prints the results in a formatted way.
+    '''
+    urls = ['https://alu-intranet.hbtn.io/status', 'http://0.0.0.0:5050/status']
 
-if __name__ == '__main__':
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        print("Body response:")
-        html = response.read()
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode('utf-8'))
+    for url in urls:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            data = response.read()
+            info = f"Body response for {url}:\n\t- type: {type(data)}\n\t- \
+content: {data}\n\t- utf8 content: {data.decode('utf-8')}"
+            print(info)
