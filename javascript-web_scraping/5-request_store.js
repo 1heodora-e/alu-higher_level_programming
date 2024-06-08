19103411d52af6bd7573626c12c1fc297e33b085
1heodora-e <t.egbunike@alustudent.com>
2024-06-08 15:07:05 -0700

    Yes

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = body;
-    fs.writeFileSync(process.argv[3], data, 'utf8');
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
